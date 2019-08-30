c3c092d540ba8440dc568155cb00462648af6743
Glitch (znw-gg) <none>
2019-08-30 11:14:05 +0000

    proper loader

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,12 @@
 /**!checkes6 by Blubbll*/
 var es6 = void 0 !== Array.from && void 0 !== Array.of && void 0 !== Math.acosh && void 0 !== Math.hypot && void 0 !== Math.imul && void 0 !== Number.isInteger && void 0 !== Number.isNaN && void 0 !== Number.EPSILON && void 0 !== Object.assign && void 0 !== Promise && void 0 !== Proxy && void 0 !== Map && void 0 !== Set && void 0 !== WeakMap && void 0 !== WeakSet && void 0 !== Symbol;
-var angular = window.angular;
+if (!es6) {
+    alert("Your browser is too shitty, please update it.");
+    throw ("browser too old.")
+}
+//imports
+const angular = window.angular,
+    swal = window.swal;
 //https://stackoverflow.com/a/23176223
 var showOffline = function() {
     var status = (
@@ -40,12 +46,41 @@ socket.onclose = function(message) {
 "use strict";
 [function() {
     var $root = {};
-    var app = angular.module('znwgg', ["xeditable", "ngRoute"]);
-    app.controller('app', function($scope) {
-        $scope.user = "test"
+    window.root = $root;
+    //$root.currentuser = {name: "test", id: "43432-432432-432432"}
+
+
+
+    var loader = setInterval(function() {
+
+
+        if (null === document.documentElement.innerText.match(/{{.*}}/g)) {
+            $(".loader").remove();
+            $("#wrapper")[0].setAttribute("style", null);
+        }
+        clearInterval(loader);
+
+    }, 500);
+
+    $root.logout = function() {
+        console.log("bye")
+    }
+    var app = angular.module('znwgg', ['xeditable', 'ngRoute', 'ngAnimate']);
+    app.controller('app', function($scope, $rootScope) {
+
+        $root.setUser = function(user) {
+            $scope.user = user;
+        }
+
+        $root.updateGui = function() {
+            $scope.$digest();
+        }
+
     });
     app.controller('gamechanger', function($scope, $filter) {
-        $scope.user = {};
+        $scope.user = {
+            game: 'none'
+        };
         //$scope.user = {
         //  game: 'mc'
         //}; 
@@ -70,9 +105,14 @@ socket.onclose = function(message) {
             var selected = $filter('filter')($scope.games, {
                 value: $scope.user.game
             });
-            if (selected.length === 1 && $scope.user.game !== selected) {
-                $scope.user.game = selected;
-                console.log(selected[0].value)
+            if (selected.length === 0)
+                $('.editable-controls>select>option[value="?"]').text("Not set");
+
+            //option
+            if (selected.length === 1 && $root.currentGame !== selected[0].value) {
+                var val = selected[0].value;
+                $scope.user.game = val;
+                $root.currentGame = val;
             }
             return ($scope.user.game && selected.length) ? selected[0].value : 'Not set';
         };
@@ -85,8 +125,8 @@ socket.onclose = function(message) {
          */
         $routeProvider.when('/:page/:name*', {
             templateUrl: function(urlattr) {
-                return $base + '/' + 
-                  urlattr.page + '/' + urlattr.name + '.htm';
+                return $base + '/' +
+                    urlattr.page + '/' + urlattr.name + '.htm';
             },
         }).when('/:page', {
             templateUrl: function(urlattr) {
@@ -96,10 +136,9 @@ socket.onclose = function(message) {
     });
     //Nach ngRoute-Navigation fixedNav refreshen
     app.controller('views', function($scope, $location, $rootScope, $log, $route, $http) {
-        //$.refreshView = function () {
-        //    $(".toast").toast("hide").find(".toast-body").html(String.Empty);
-        //    $route.reload();
-        //};
+        $root.refreshView = function() {
+            $route.reload();
+        };
         //Wenn View geladen ist
         $scope.$watch('$viewContentLoaded', function() {
             //$scope.loaded === void 0 ? [$("[data-loading-text]").remove(), $scope.loaded = true] : [];
@@ -114,13 +153,33 @@ socket.onclose = function(message) {
             //Navbutton, der mit View übereinstimmt, aktiv machen
             $($.navElem + '[data-href="' + view + '"]').addClass("active");
         };
-        setTimeout($root.gotoView("/account/overview"), 999)
+        //setTimeout($root.gotoView("/account/overview"), 999)
         //Aktuellen View-Namen erhalten (Anwendungspfad)
         $root.getView = function() {
             return $location.$$path;
         };
         //Bei Navigation
         $rootScope.$on("$locationChangeStart", function(event, next, current) {
+
+            $("#sidebar-wrapper>nav").addClass("loading");
+            switch (next.split('#!')[1]) {
+                case '/logout':
+                    {
+                        event.preventDefault();
+                        $("#sidebar-wrapper>nav").removeClass("loading");
+                        swal({
+                            title: "Are you sure?",
+                            text: "Please confirm your logout.",
+                            icon: "warning",
+                            buttons: true,
+                            dangerMode: true,
+                        })
+                        .then((doLogout) => {
+                            if (doLogout) $root.logout();
+                        });
+                    }
+                    break;
+            }
             //$.fixUrl();
         });
         //Routenwechsel schlägt fehl.
@@ -130,8 +189,10 @@ socket.onclose = function(message) {
         });
         //Bei erfolgreichem Routenwechsel
         $rootScope.$on("$routeChangeSuccess", function(event, current, previous) {
+
             //wohin gehen wir?
-            console.log(current.$$route)
+            //console.log(current.$$route)
+            $root.refreshNavi();
             switch (current.$$route.originalPath) {
                 case "/":
                     {
@@ -151,29 +212,36 @@ socket.onclose = function(message) {
     });
     //Nach ngRoute-Navigation fixedNav refreshen
     app.controller('nav', ['$scope', '$http', function($scope, $http) {
-        function getFromServer() {
+
+        $root.refreshNavi = function() {
             var path = $root.getView();
-            $scope.path = path;
-            $scope.links = [];
             if (path === "/") path = "/start";
+
             //only first layer
             path = '/' + path.split("/")[1];
+
+            if ($root.currentGame !== void 0) path = '/' + $root.currentGame;
+
+            $scope.path = path;
+            $scope.links = [];
             $http({
                 method: 'GET',
                 url: '/menu' + path + ".json"
             }).then(function(res) {
                 for (var i in res.data) {
                     var link = res.data[i];
-                
-                  
-                  
                     $scope.links.push({
                         href: link.href.replace("$", path.split("/")[1]),
                         text: link.text
-                    })
+                    });
+                    setTimeout(function() {
+                        $("#sidebar-wrapper>nav").removeClass("loading");
+                    }, 50)
                 }
             }, function(error) {});
         };
-        setTimeout(getFromServer, 0);
+
+
+
     }]);
 }()];
